a7a88c20a9e1edb66dc07caf824cb654ac6b9b25
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2023-01-09 17:50:06 -0600

    Update rammerhead.ext.js

--- assets/rammerhead.ext.js
+++ assets/rammerhead.ext.js
@@ -1,8 +1,7 @@
-let o = document.createElement("script");
-o.src = "https://rh.therealgeodash.workers.dev/require.js";
+let j = "rh.settings.lhost.dev", o = document.createElement("script"), o2 = document.createElement("script");
+o.src = `https://${j}/require.js`;
 o.id = "require";
 document.head.appendChild(o);
-let o2 = document.createElement("script");
-o2.src = "https://rh.therealgeodash.workers.dev/injectRammerhead.js";
+o2.src = `https://${j}/injectRammerhead.js`;
 o2.id = "rammerhead-ext";
 document.head.appendChild(o2);
