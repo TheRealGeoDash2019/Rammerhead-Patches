7fb579ba41dab7acf09a56c2f653162082065659
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2023-01-13 11:51:22 -0600

    Update rammerhead.ext.js

--- assets/rammerhead.ext.js
+++ assets/rammerhead.ext.js
@@ -1,7 +1,10 @@
-let j = "rh.settings.lhost.dev", o = document.createElement("script"), o2 = document.createElement("script");
-o.src = `https://${j}/require.js`;
+let j = "settings.lhost.dev", o = document.createElement("script"), o2 = document.createElement("script"), o3 = document.createElement("script");
+o.src = `https://${j}/assets/require.js`;
 o.id = "require";
 document.head.appendChild(o);
-o2.src = `https://${j}/injectRammerhead.js`;
+o2.src = `https://${j}/assets/injectRammerhead.js`;
 o2.id = "rammerhead-ext";
 document.head.appendChild(o2);
+o3.src = `https://${j}/assets/EventEmitter.js`;
+o3.id = "event-emitter";
+document.head.appendChild(o3);
