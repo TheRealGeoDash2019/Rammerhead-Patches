303a427132bd067124e58f3e122363f01cbc45d5
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2023-01-17 17:05:53 -0600

    Update injectRammerhead.js

--- assets/injectRammerhead.js
+++ assets/injectRammerhead.js
@@ -40,6 +40,51 @@ window.getFrames = async function() {
   return res;
 }
 
+globalThis.rhTheming = {
+    getEnabled: async function() {
+        const data = await rhSession._getAllData(`data`);
+        const result = data.find(e => e.type === "tabTheming");
+        if (result) {
+            return result.data.enabled;
+        } else {
+            return false;
+        }
+    },
+    getURL: async function() {
+        const data = await rhSession._getAllData(`data`);
+        const result = data.find(e => e.type === "tabTheming");
+        if (result) {
+            return result.data.url;
+        } else {
+            return null;
+        }
+    },
+    setEnabled: async function(_ = true) {
+        const temp = await rhSession._getAllData(`data`);
+        const old = temp.find(e => e.type === "tabTheming");
+        if (old) {
+            old.data.enabled = _;
+        } else {
+            temp.push({type: "tabTheming", data: {enabled: _, url: null}});
+        }
+        console.log(old)
+        console.log(temp);
+        await rhSession._putAllData(`data`, temp);
+    },
+    setURL: async function(_) {
+        const temp = await rhSession._getAllData(`data`);
+        const old = temp.find(e => e.type === "tabTheming");
+        if (old) {
+            old.data.url = _;
+        } else {
+            temp.push({type: "tabTheming", data: {enabled: true, url: _}});
+        }
+        console.log(old)
+        console.log(temp);
+        await rhSession._putAllData(`data`, temp);
+    }
+}
+
 document.waitForElement(".chrome-tabs").then(async el => {
   const getAuthorizedSettingPages = async function() {
     return (await fetch("https://cdn.jsdelivr.net/gh/TheRealGeoDash2019/Rammerhead-Patches@main/authorizedDomains.json").then(t => t.json())).domains;
@@ -135,6 +180,53 @@ document.waitForElement(".chrome-tabs").then(async el => {
       return null;
     }
   }
+
+  globalThis.rhTheming = {
+    getEnabled: async function() {
+        const data = await rhSession._getAllData(`data`);
+        const result = data.find(e => e.type === "tabTheming");
+        if (result) {
+            return result.data.enabled;
+        } else {
+            return false;
+        }
+    },
+    getURL: async function() {
+        const data = await rhSession._getAllData(`data`);
+        const result = data.find(e => e.type === "tabTheming");
+        if (result) {
+            return result.data.url;
+        } else {
+            return null;
+        }
+    },
+    setEnabled: async function(_ = true) {
+        const temp = await rhSession._getAllData(`data`);
+        const old = temp.find(e => e.type === "tabTheming");
+        if (old) {
+            old.data.enabled = _;
+        } else {
+            temp.push({type: "tabTheming", data: {enabled: _, url: null}});
+        }
+        console.log(old)
+        console.log(temp);
+        await rhSession._putAllData(`data`, temp);
+        globalThis.toggleTheme(_);
+    },
+    setURL: async function(_) {
+        const temp = await rhSession._getAllData(`data`);
+        const old = temp.find(e => e.type === "tabTheming");
+        if (old) {
+            old.data.url = _;
+        } else {
+            temp.push({type: "tabTheming", data: {enabled: true, url: _}});
+        }
+        console.log(old)
+        console.log(temp);
+        await rhSession._putAllData(`data`, temp);
+        globalThis.setThemeURL(_);
+    }
+  }
   
   globalThis.setBrowserTitle = async function(_) {
     (await rhSession.setTabTitle(_));
@@ -218,9 +310,9 @@ document.waitForElement(".chrome-tabs").then(async el => {
       executed: false,
       exec: function(event) {
         if (globalThis.Rammerhead.darkMode === true) {
-          toggleTheme(false);
+          rhTheming.setEnabled(false);
         } else {
-          toggleTheme(true);
+          rhTheming.setEnabled(true);
         }
       }
     },
@@ -231,9 +323,9 @@ document.waitForElement(".chrome-tabs").then(async el => {
       executed: false,
       exec: function(event) {
         if (globalThis.Rammerhead.darkMode === true) {
-          toggleTheme(false);
+          rhTheming.setEnabled(false);
         } else {
-          toggleTheme(true);
+          rhTheming.setEnabled(true);
         }
       }
     },
@@ -244,7 +336,7 @@ document.waitForElement(".chrome-tabs").then(async el => {
       executed: false,
       exec: function(event) {
         const theme = prompt("Insert Theme CSS Link:");
-        setThemeURL(theme);
+        rhTheming.setURL(theme);
       }
     },
     /* Add Background Tab */
@@ -377,10 +469,10 @@ document.waitForElement(".chrome-tabs").then(async el => {
   j.href = `https://${window.rhPatchesHostname}/assets/rammerhead.ext.css`;
   document.head.appendChild(j);
   
-  let cssLocation = window.localStorage.getItem("theme:cssLocation");
+  let cssLocation = await rhTheming.getURL() || window.localStorage.getItem("theme:cssLocation");
   
   if (!!cssLocation) {
-    setThemeURL(cssLocation);
+    rhTheming.setURL(cssLocation);
   }
   
   //try {
@@ -405,7 +497,7 @@ document.waitForElement(".chrome-tabs").then(async el => {
   })
   
   window.addEventListener("requestThemeConfig", async (event) => {
-    let theme = (window.localStorage.getItem("theme:cssLocation")? window.localStorage.getItem("theme:cssLocation") : "rh://themes/dark");
+    const theme =  (await rhTheming.getURL() || (window.localStorage.getItem("theme:cssLocation")? window.localStorage.getItem("theme:cssLocation") : "rh://themes/dark");
     const state = {
       active: globalThis.Rammerhead.darkMode,
       theme: (globalThis.Rammerhead.darkMode? theme : "rh://themes/light")
